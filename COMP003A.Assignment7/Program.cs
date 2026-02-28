29e9f06cb73e4e552aca28596c54ae0afee0b737
evanbern59 <evansantos1202@gmail.com>
2026-02-27 22:07:30 -0800

    added student scores list

--- COMP003A.Assignment7/Program.cs
+++ COMP003A.Assignment7/Program.cs
@@ -8,6 +8,12 @@
              * Theme A: Student Score Analyzer
              * 
              */
+
+            List<int> studentScores = new List<int> { 99, 85, 59, 93, 87 };
+
+            bool running = true;
+
+
         }
     }
 }
